5abdcf4f22227c3f138841b309eefc2ec0d89d31
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-12-12 21:22:55 -0700

    added the service worker so assets will load offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,5 @@
+const e = require("express");
+
 const APP_PREFIX = 'BudgetTracker-'
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
@@ -9,10 +11,32 @@ const FILES_TO_CACHE = [
    './js/index.js'
 ]
 
-self.addEventListener('install', function(event) {
-   event.waitUntil(
-      caches.open(CACHE_NAME).then(function (cache) {
+self.addEventListener('fetch', function(event) {
+   e.respondWith(caches.match(e.request).then(function (request) {
+      return request || fetch(e.request);
+   }));
+});
+
+self.addEventListener('install', function (event) {
+   event.waitUntil(caches.open(CACHE_NAME).then(function (cache) {
       console.log('installing cache : ' + CACHE_NAME);
       return cache.addAll(FILES_TO_CACHE);
-    }));
+   }));
+});
+
+self.addEventListener('activate', function (event) {
+   event.waitUntil(caches.keys().then(function (keyList) {
+      let cacheKeepList = keyList.filter(function (key) {
+         return key.indexOf(APP_PREFIX);
+      });
+
+      cacheKeepList.push(CACHE_NAME);
+
+      return Promise.all(keyList.map(function (key, i) {
+         if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+         }
+      }));
+   }));
 });
